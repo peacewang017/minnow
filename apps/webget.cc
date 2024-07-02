dac224d1591cbd233bf6236cb06b2399a3b2bc08
peacewang017 <peacewang001@gmail.com>
2024-07-02 13:00:51 +0800

    finished lab0: webget && bytestream

--- apps/webget.cc
+++ apps/webget.cc
@@ -4,43 +4,63 @@
 #include <iostream>
 #include <span>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-void get_URL( const string& host, const string& path )
+void get_URL(const string &host, const string &path)
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+	cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+	// cerr << "Warning: get_URL() has not been implemented yet.\n";
+
+	ostringstream request;
+	request << "GET " << path << " HTTP/1.1\r\n"
+		<< "Host: " << host << "\r\n"
+		<< "Connection: close\r\n\r\n";
+
+	TCPSocket socket_tcp{};
+	socket_tcp.connect(Address(host, "http"));
+	socket_tcp.write(request.str()); // 将 ostringstream 转为 string
+	socket_tcp.shutdown(SHUT_WR); //SHUT_WR: 不再发送数据
+
+	string buffer;
+	while (!socket_tcp.eof()) {
+		socket_tcp.read(buffer);
+		cout << buffer;
+	}
+
+	socket_tcp.close();
 }
 
-int main( int argc, char* argv[] )
+int main(int argc, char *argv[])
 {
-  try {
-    if ( argc <= 0 ) {
-      abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
-    }
+	try {
+		if (argc <= 0) {
+			abort(); // For sticklers: don't try to access argv[0] if argc <= 0.
+		}
 
-    auto args = span( argv, argc );
+		auto args = span(argv, argc);
 
-    // The program takes two command-line arguments: the hostname and "path" part of the URL.
-    // Print the usage message unless there are these two arguments (plus the program name
-    // itself, so arg count = 3 in total).
-    if ( argc != 3 ) {
-      cerr << "Usage: " << args.front() << " HOST PATH\n";
-      cerr << "\tExample: " << args.front() << " stanford.edu /class/cs144\n";
-      return EXIT_FAILURE;
-    }
+		// The program takes two command-line arguments: the hostname and "path" part of the URL.
+		// Print the usage message unless there are these two arguments (plus the program name
+		// itself, so arg count = 3 in total).
+		if (argc != 3) {
+			cerr << "Usage: " << args.front() << " HOST PATH\n";
+			cerr << "\tExample: " << args.front()
+			     << " stanford.edu /class/cs144\n";
+			return EXIT_FAILURE;
+		}
 
-    // Get the command-line arguments.
-    const string host { args[1] };
-    const string path { args[2] };
+		// Get the command-line arguments.
+		const string host{ args[1] };
+		const string path{ args[2] };
 
-    // Call the student-written function.
-    get_URL( host, path );
-  } catch ( const exception& e ) {
-    cerr << e.what() << "\n";
-    return EXIT_FAILURE;
-  }
+		// Call the student-written function.
+		get_URL(host, path);
+	} catch (const exception &e) {
+		cerr << e.what() << "\n";
+		return EXIT_FAILURE;
+	}
 
-  return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -2,64 +2,79 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream(uint64_t capacity)
+    : buffer(), capacity_(capacity)
+{
+}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return (!is_open);
 }
 
-void Writer::push( string data )
+void Writer::push(string data)
 {
-  // Your code here.
-  (void)data;
-  return;
+    // Your code here.
+    if (!is_open) return;
+    for (auto ch : data) {
+        if (buffer.size() >= capacity_) return;
+        buffer.push(ch);
+        nums_bytes_pushed++;
+    }
+    return;
 }
 
 void Writer::close()
 {
-  // Your code here.
+    // Your code here.
+    is_open = false;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return (capacity_ - (uint64_t)buffer.size());
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return nums_bytes_pushed;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return (bool)(!is_open && buffer.empty());
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return nums_bytes_poped;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    if (!buffer.empty()) {
+        return string_view(&buffer.front(), sizeof(char));
+    }
+    return std::string_view();
 }
 
-void Reader::pop( uint64_t len )
+void Reader::pop(uint64_t len)
 {
-  // Your code here.
-  (void)len;
+    for (uint64_t i = 0; i < len; i++) {
+        if (buffer.empty()) return;
+        buffer.pop();
+        nums_bytes_poped++;
+    }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+    // Your code here.
+    return (uint64_t)buffer.size();
 }
